ef48a70d7ad998f33a725d2c0a2b41582c63db10
lmbcosta <lmbcosta@hotmail.com>
2018-03-07 21:25:47 +0000

    Initial Commit

---
+++ ProgressChartCircleView/ProgressChartCircleView.h
@@ -0,0 +1,19 @@
+//
+//  ProgressChartCircleView.h
+//  ProgressChartCircleView
+//
+//  Created by Luis  Costa on 07/03/18.
+//  Copyright © 2018 Luis  Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressChartCircleView.
+FOUNDATION_EXPORT double ProgressChartCircleViewVersionNumber;
+
+//! Project version string for ProgressChartCircleView.
+FOUNDATION_EXPORT const unsigned char ProgressChartCircleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressChartCircleView/PublicHeader.h>
+
+
